0d307ef546ad983b6cf46ec0b01b1bd5583266cc
limkkk1 <296290218@qq.com>
2025-09-30 15:31:06 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -5,13 +5,16 @@
 export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
-      <span className='mr-1'>Powered by</span>
+      <span className="mr-1">Powered by</span>
       <a
-        href='https://linktr.ee/openbuild' // 改成你的链接
-        className='underline'>
+        href="https://linktr.ee/openbuild" // 你的链接
+        className="underline"
+        target="_blank" // 点击在新标签打开
+        rel="noopener noreferrer"
+      >
         Openbuild&Ccool
       </a>
       .
     </div>
-  )
+  );
 }
