ad28ed62a388f2af21667bcba2db0705c44f0f70
limkkk1 <296290218@qq.com>
2025-09-30 15:30:13 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -7,7 +7,7 @@ export default function PoweredBy(props) {
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
-        href='https://linktr.ee/openbuild' // 改成你想要的链接
+        href='https://linktr.ee/openbuild' // 改成你的链接
         className='underline'>
         Openbuild&Ccool
       </a>
