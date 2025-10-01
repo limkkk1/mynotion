cbaac1bbcd281bf997fba070344573a212dc3fbf
limkkk1 <296290218@qq.com>
2025-10-01 16:36:31 +0800

    Update Footer.js

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -52,9 +52,9 @@ const Footer = ({ siteInfo }) => {
       <div className='text-xs font-serif'>
         Powered By{' '}
         <a
-          href='https://github.com/tangly1024/NotionNext'
+          href='https://openbuild.sg.larksuite.com'
           className='underline text-gray-500 dark:text-gray-300'>
-          NotionNext {siteConfig('VERSION')}
+          openbuild
         </a>
       </div>
       {/* SEO title */}
